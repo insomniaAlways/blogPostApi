736e74ccaa03216b945aacfdbc48272e67e9d057
trdp30 <trdp30@gmail.com>
2018-10-19 11:44:44 +0530

    fixed CORS issue

--- src/blogPost/Service.java
+++ src/blogPost/Service.java
@@ -5,15 +5,32 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path; 
 import javax.ws.rs.Produces; 
 import javax.ws.rs.core.MediaType; 
+import java.io.IOException;
 
-@Path("service") 
-public class Service {	
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+
+@Path("/service")
+@Provider
+public class Service implements ContainerResponseFilter  {
+	
+	@Override
+	   public void filter(final ContainerRequestContext requestContext,
+	                      final ContainerResponseContext cres) throws IOException {
+	      cres.getHeaders().add("Access-Control-Allow-Origin", "*");
+	      cres.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+	      cres.getHeaders().add("Access-Control-Allow-Credentials", "true");
+	      cres.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+	      cres.getHeaders().add("Access-Control-Max-Age", "1209600");
+	   }
 	LibraryDao readLibrary = new LibraryDao();
-	@GET 
+	@GET
+	
 	@Path("/library") 
 	@Produces(MediaType.APPLICATION_JSON) 
 	public List<Library> getUsers(){ 
 	   return readLibrary.getAllUsers();
 	}
 }  
-	
